a465d9207126429dc9418d29babec692043e2906
Katy Farmer <dreadpiratekaty@gmail.com>
2019-03-04 19:38:10 -0800

    Basic flight and flower animation

---
+++ flower.js
@@ -0,0 +1,36 @@
+import { Container, Sprite } from "pixi.js"
+import TWEEN from "@tweenjs/tween.js"
+import {Tween} from "@tweenjs/tween.js"
+
+const daisyTopURL = require('./images/daisy-head.png')
+const daisyBottomURL = require('./images/daisy-bottom.png')
+
+export default class Flower extends Container {
+  constructor(x, y) {
+    super()
+    this.x = x
+    this.interactive = true
+    this.interactiveChildren = true
+    this.topImage = Sprite.fromImage(daisyTopURL)
+    this.bottomImage = Sprite.fromImage(daisyBottomURL)
+    this.bottomImage.y = this.topImage.height
+    this.addChild(this.topImage)
+    this.addChild(this.bottomImage)
+    this.y = y - this.height
+    this.buttonMode = true
+  }
+
+  animateLanding() {
+    new Tween(this.topImage.position)
+    .to({y: 2}, 50)
+    .easing(TWEEN.Easing.Exponential.InOut)
+    .start()
+  }
+
+  animateLeaving() {
+    new Tween(this.topImage.position)
+    .to({y: 0}, 50)
+    .easing(TWEEN.Easing.Exponential.InOut)
+    .start()
+  }
+}
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import * as PIXI from 'pixi.js'
+import Bump from 'bump.js'
 import TWEEN from "@tweenjs/tween.js"
 import {Tween} from "@tweenjs/tween.js"
+import Flower from "./flower"
 const app = new PIXI.Application({width: 800, height: 600})
+const bump = new Bump(PIXI)
+const graphics = new PIXI.Graphics()
 
-const daisyURL = require('./images/daisy.png')
+const daisyTopURL = require('./images/daisy-head.png')
+const daisyBottomURL = require('./images/daisy-bottom.png')
 const beeURL = require('./images/bumblebee.png')
 
 //make canvas the size of browser window
@@ -18,57 +23,57 @@ document.body.appendChild(app.view)
 
 PIXI.loader
   .add([
-    daisyURL,
+    daisyTopURL,
+    daisyBottomURL,
     beeURL
   ])
   .load( () => {
+
     let bee = PIXI.Sprite.fromImage(beeURL)
-
-    bee.scale.x = 0.125
-    bee.scale.y = 0.125
-
+    let currentDaisy
+    bee.interactive = true
 
     bee.x = app.screen.width - bee.width
 
-
-
     function onClick(sprite) { return () => {
+      if (currentDaisy) {
+        currentDaisy.animateLeaving()
+      }
       new Tween(bee.position)
-      .to({ x:sprite.x, y: sprite.y - 50 }, 10000)
-      .yoyo(true)
-      .easing(TWEEN.Easing.Quadratic.Out)
-      .repeat(Infinity)
+      .to({ x:sprite.x + 50, y: sprite.y - 30 }, 1000)
+      .easing(TWEEN.Easing.Circular.Out)
+      .onComplete( () => {
+        currentDaisy = sprite
+        sprite.animateLanding()
+      })
       .start()
       }
     }
 
-    // for (var i = 0; i < 25; i++) {
-    // var bunny = new PIXI.Sprite(texture);
-    // bunny.anchor.set(0.5);
-    // bunny.x = (i % 5) * 40;
-    // bunny.y = Math.floor(i / 5) * 40;
     let daisies = new PIXI.Container()
     daisies.interactive = true
     daisies.interactiveChildren = true
 
     for (var i = 0; i < 3; i++) {
-      let daisy = PIXI.Sprite.fromImage(daisyURL)
+      let daisy = new Flower((i % 5) * 300, app.screen.height)
 
-      daisy.scale.x = 0.25
-      daisy.scale.y = 0.25
-
-      daisy.y = app.screen.height - daisy.height
-      daisy.interactive = true
-      daisy.buttonMode = true
       daisy.on('pointerdown', onClick(daisy))
 
-      daisy.x = (i % 5) * 150
-      daisy.y = app.screen.height - daisy.height
       daisies.addChild(daisy)
     }
 
+
+//status bar
+
+    let statusBar = new PIXI.Container()
+    // graphics.lineStyle(2, 0xffe446);
+    // graphics.beginFill(0xffe446, 0.25);
+    // graphics.drawRoundedRect(10, 10, 400, 20, 10);
+    // graphics.endFill();
+
     app.stage.addChild(daisies)
     app.stage.addChild(bee)
+    app.stage.addChild(graphics)
 
     app.ticker.add(() => {
       TWEEN.update()
